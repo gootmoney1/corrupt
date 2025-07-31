6ec8404fb2e9ad0ea3ca6134e805584495b1b660
gootmoney1 <sloanngutierrez@gmail.com>
2025-07-31 09:47:22 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="corrupt",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[],
     python_requires=">=3.6,<=3.13.5",
